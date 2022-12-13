43c64f7e2b1f03e1b943c5b5eaf100bfe3cbd8d4
Yeonsoo1216 <79428586+Yeonsoo1216@users.noreply.github.com>
2022-12-13 20:50:20 +0900

    디자인 및 버튼생성

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -5,20 +5,30 @@ const Calendar = () => {
   
   const currentYear = new Date(date).getFullYear();
   const currentMonth = new Date(date).getMonth() + 1;
+  const currentDate = new Date(date).getDate();
 
   const firstDay = new Date(date.setDate(1)).getDay();
   const lastDay = new Date(currentYear, currentMonth, 0).getDate();
 
-  
-
   return (
     <div>
+      <div className="flex aic jcsb">
+        <h1 className="current">{currentYear}년 {currentMonth}월</h1>
+        <div>
+          <div style={{marginRight: 16}} className="btn">이전달</div>
+          <div className="btn" >다음달</div>
+        </div>
+      </div>
+
       <div className="Calendar">
         {new Array(firstDay + lastDay).fill(null).map((v, i) => {
           i -= firstDay;
           return (
             <div className={i < 1 ? "hidden-date" : ""}>
               {i}
+              <div>
+                {i == currentDate ? "ToDay" : ""}
+              </div>
             </div>
           );
         })}

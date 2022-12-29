5258748375a5aef386c4900c974f2bf2b99d9e3a
Yeonsoo1216 <79428586+Yeonsoo1216@users.noreply.github.com>
2022-12-29 19:24:39 +0900

    date

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -14,10 +14,6 @@ const Calendar = () => {
     <div>
       <div className="flex aic jcsb">
         <h1 className="current">{currentYear}년 {currentMonth}월</h1>
-        <div>
-          <div style={{marginRight: 16}} className="btn">이전달</div>
-          <div className="btn" >다음달</div>
-        </div>
       </div>
 
       <div className="Calendar">
@@ -26,8 +22,9 @@ const Calendar = () => {
           return (
             <div className={i < 1 ? "hidden-date" : ""}>
               {i}
-              <div>
-                {i == currentDate ? "ToDay" : ""}
+              <br></br>
+              <div className="day">
+                {i == currentDate ? "ToDay" : "　"}
               </div>
             </div>
           );
